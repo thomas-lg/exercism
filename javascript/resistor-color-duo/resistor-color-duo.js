c345ec6528a9f1b4bacebdab5ab065bdb0941cb3
Thomas Le Gougaud <tom.legougaud@gmail.com>
2019-07-15 12:00:42 +1200

    resistant color duo exercise

---
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -0,0 +1,16 @@
+export const value = colors => 
+   +`${COLORS.indexOf(colors[0])}${COLORS.indexOf(colors[1])}`;
+
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white'
+];
