6a4298d9fac34f74cb1f6ae6ee5fb03fd339fe33
Le Gougaud Thomas <tom.legougaud@gmail.com>
2020-01-27 18:19:15 +0100

    reformat

--- javascript/collatz-conjecture/collatz-conjecture.js
+++ javascript/collatz-conjecture/collatz-conjecture.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Collatz Conjecture' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const steps = n => {
   if (n <= 0) {
     throw "Only positive numbers are allowed";
